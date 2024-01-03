519e6f15dc0dd72723172b2b378cb2c44043ba1f
Nitharsha99 <nitharshathev@gmail.com>
2024-01-04 00:57:10 +0530

    remove unwanted codes

--- Student_projectAPI/Program.cs
+++ Student_projectAPI/Program.cs
@@ -9,9 +9,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("connectionString"), sqlServerOptions =>
         sqlServerOptions.EnableRetryOnFailure(2, TimeSpan.FromSeconds(10), null)));
 
-//builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-//    .AddDefaultTokenProviders().AddDefaultUI()
-//    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,16 +17,6 @@ builder.Services.AddSwaggerGen();
 
 //builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //options.UseSqlServer(builder.Configuration.GetConnectionString("connectionString")));
-
-
-
-//builder.Services.AddCors((corsoptions) =>
-//{
-//    corsoptions.AddPolicy("Mypolicy", (policyoptions) =>
-//    {
-//        policyoptions.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
-//    });
-//});
 
 var app = builder.Build();
 
